705929a407ea4ac8d4ad4131f94fd12adff6b525
root <root@DESKTOP-3A6NM83.localdomain>
2023-10-20 20:14:35 +0200

    update cutrun metadata

--- GEO/cp_fastq.sh
+++ GEO/cp_fastq.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-cp /shared/projects/xci/homo_sapiens/stem_cells/charbel/2023/gro_seq/fastq/raw/* gro_seq/
+cp /shared/projects/xci/homo_sapiens/stem_cells/charbel/paper/cutrun/cutrun_d725/D725C01_trimmed.*.fastq.gz cutrun/
+cp /shared/projects/xci/homo_sapiens/stem_cells/charbel/paper/cutrun/cutrun_d725/D725C04_trimmed.*.fastq.gz cutrun/
+cp /shared/projects/xci/homo_sapiens/stem_cells/charbel/paper/cutrun/cutrun_d725/D725C05_trimmed.*.fastq.gz cutrun/
+cp /shared/projects/xci/homo_sapiens/stem_cells/charbel/paper/cutrun/cutrun_d725/D725C06_trimmed.*.fastq.gz cutrun/
+cp /shared/projects/xci/homo_sapiens/stem_cells/charbel/paper/cutrun/cutrun_d725/D725C07_trimmed.*.fastq.gz cutrun/
+cp /shared/projects/xci/homo_sapiens/stem_cells/charbel/paper/cutrun/cutrun_d725/D725C08_trimmed.*.fastq.gz cutrun/
+cp /shared/projects/xci/homo_sapiens/stem_cells/charbel/paper/cutrun/cutrun_d725/D725C11_trimmed.*.fastq.gz cutrun/
+cp /shared/projects/xci/homo_sapiens/stem_cells/charbel/paper/cutrun/cutrun_d725/D725C12_trimmed.*.fastq.gz cutrun/
+cp /shared/projects/xci/homo_sapiens/stem_cells/charbel/paper/cutrun/cutrun_d725/D725C13_trimmed.*.fastq.gz cutrun/
+cp /shared/projects/xci/homo_sapiens/stem_cells/charbel/paper/cutrun/cutrun_d725/D725C14_trimmed.*.fastq.gz cutrun/
 
 exit
